802024fc805204eb2c75238ef423358f63ea249a
fearlpss <fearlpss@gmail.com>
2026-09-17 10:28:21 -0500

    Add files via upload

--- route.js
+++ route.js
@@ -1,3 +1,6 @@
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
 import { getState, setState, hasPersistentStore, finalizeState } from '../../../lib/store';
 
 const OWNER_EMAIL = 'threatenn@outlook.com';
